bf77644560dcfaa61c3402f3808da3995088dbfd
Akash Nagalwade <nagalwade789@gmail.com>
2025-01-30 20:54:52 +0530

    program for prime isnumber

---
+++ primeNumber.java
@@ -0,0 +1,18 @@
+package Interview;
+
+public class primeNumber {
+    static String prime(int n){
+
+        for(int i = 2;i <n ; i++) {
+            if(n%i ==0) {
+                return "Not prime";
+            }
+        }
+        return "prime";
+    }
+    public static void main(String[] args) {
+        int n = 11;
+        System.out.println(prime(n));
+    }
+
+}
